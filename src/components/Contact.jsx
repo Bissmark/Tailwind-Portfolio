a61eafb79bd6ff06e57470b71bf7c2a514adec7f
Christopher Holt <holt.christopher1@gmail.com>
2024-06-30 23:14:12 +1000

    added some of the contact me page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const Contact = () => {
     return (
         <div>
             <h1>Contact</h1>
-            <p>Here is my contact info</p>
+            <p>I am available for hire and excited for opportunities</p>
+
+            <h1>Email:<Link to="mailto:">holt.christopher1@gmail.com</Link></h1>
+            <h1>LinkedIn: <Link to="https://www.linkedin.com/in/cjholt/">CJ Holt</Link></h1>
+            <h1>Github:<Link to="https://github.com/Bissmark">Bissmark</Link></h1>
+            <h1>Youtube:<Link to="https://www.youtube.com/channel/UCkiCA2AtCdRxRd_G-svQ2PA">CJ Holt</Link></h1>
+            
         </div>
     );
 }
